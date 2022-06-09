0f5237f7ebd5c9cbf55e752c7f28a6f53f701e5a
Ian Rudy <107053197+YellowTeamRobot@users.noreply.github.com>
2022-06-09 10:32:16 -0400

    Add files via upload

---
+++ AAS_Data.h
@@ -0,0 +1,130 @@
+#ifndef __AAS_DATA__
+#define __AAS_DATA__
+
+#include "AAS.h"
+
+#if AAS_VERSION != 0x113
+#error AAS version does not match Conv2AAS version
+#endif
+
+AAS_BEGIN_DECLS
+
+extern const AAS_s8* const AAS_DATA_SFX_START_aagh;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_aagh;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_AnotherStormAbated;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_AnotherStormAbated;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_Applejack;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_Applejack;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_BeepBopBoop;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_BeepBopBoop;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_Booyah3;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_Booyah3;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_ClearSkies2;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_ClearSkies2;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_CloudPuff;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_CloudPuff;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_DangIt;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_DangIt;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_Drats;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_Drats;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_Fluttershy;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_Fluttershy;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_GoodJob;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_GoodJob;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_LetsDoThis;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_LetsDoThis;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_LetsDoThis1;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_LetsDoThis1;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_LetsDoThis2;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_LetsDoThis2;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_LetsDoThis3;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_LetsDoThis3;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_LetsDoThis4;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_LetsDoThis4;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_MLPthemeIntro;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_MLPthemeIntro;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_MLPthemeLoop;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_MLPthemeLoop;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_NewRecord;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_NewRecord;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_NotKonamiGame;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_NotKonamiGame;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_Pinkie;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_Pinkie;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_Rarity;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_Rarity;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_Spike;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_Spike;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_StormcloudDefeated;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_StormcloudDefeated;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_SuperExtra;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_SuperExtra;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_ThatWasAwesome;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_ThatWasAwesome;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_Twilight;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_Twilight;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_Zap;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_Zap;
+
+extern const AAS_s8* const AAS_DATA_SFX_START_Zap2;
+
+extern const AAS_s8* const AAS_DATA_SFX_END_Zap2;
+
+AAS_END_DECLS
+
+#endif
